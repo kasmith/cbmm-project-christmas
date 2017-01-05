b44fa596937a0a4527e226ac6be00d9f060e67dd
kasmith <k2smith@mit.edu>
2017-01-05 13:47:36 -0800

    Two complex trials

---
+++ ContainmentTrials/make_json.py
@@ -0,0 +1,10 @@
+from physicsTable import *
+import os
+
+PTR_DIR = "exp_trials"
+JSON_DIR = "JSON"
+
+for fnm in os.listdir(PTR_DIR):
+    if fnm[-4:] == '.ptr':
+        tr = loadTrial(os.path.join(PTR_DIR, fnm))
+        tr.jsonify(fldir=JSON_DIR, askoverwrite=False)
